23cef3a180e646b73624b1abc9f723bbfd99a80f
NZAR <98880087+heynzar@users.noreply.github.com>
2024-12-08 22:45:18 +0100

    🚀 core : project setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-geist-sans",
+  weight: ["200", "300", "400", "500", "600", "700", "800"],
+});
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
+
+export const metadata: Metadata = {
+  title: "Simple Landing Page",
+  description: "Simple Landing Page",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className}  antialiased`}>{children}</body>
+    </html>
+  );
+}
